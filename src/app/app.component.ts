479df2c04c29ba53a19e09454879c296b0ad8200
Mengqi Hu <hu000106@algonquinlive.com>
2024-03-12 14:26:52 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faTemperatureArrowDown, faTemperatureArrowUp, faDroplet, faWind } from '@fortawesome/free-solid-svg-icons';
+import { response } from 'express';
+import { WeatherData } from './models/weather.model';
+import { WeatherService } from './services/weather.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FontAwesomeModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+  constructor(private weatherService: WeatherService){
+
+  }
+  cityName:string = 'Ottawa';
+  weatherData?: WeatherData;
+
+  ngOnInit(): void {
+    this.getWeatherData(this.cityName);
+    this.cityName = '';
+  }
+
+  onSubmit(){
+    this.getWeatherData(this.cityName);
+    this.cityName = '';
+  }
+
+  private getWeatherData(cityName:string){
+    this.weatherService.getWeatherData(cityName)
+    .subscribe({
+      next: (response)=>{
+        this.weatherData = response;
+      }
+    })    
+  }
+  
   title = 'WeatherApp';
+  faTemperatureLow = faTemperatureArrowDown;
+  faTemperatureHigh = faTemperatureArrowUp;
+  faDroplet = faDroplet;
+  faWind = faWind;
 }
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: false,
+    weatherApiBaseUrl: 'https://yahoo-weather5.p.rapidapi.com/weather',
+    XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+    XRapidAPIHostHeaderValue: 'yahoo-weather5.p.rapidapi.com',
+    XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+    XRapidAPIKeyHeaderValue: 'b33747567bmsh487facdce2019cbp1955edjsn89b9508308b2',    
+};
